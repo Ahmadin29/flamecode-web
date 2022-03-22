a209d120f72238d19b3a350eac0ea3b16d53dd63
Ahmad Macbook Air <ahmaddin211056@gmail.com>
2022-03-22 10:08:05 +0700

    dashboard layouts

---
+++ pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import DashboardLayout from '../../components/dashboard/layout'
+import styles from '../../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <DashboardLayout>
+            asd
+        </DashboardLayout>
+    )
+}
