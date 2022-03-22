1ddd9d16e1d1016ebe4c65bdaa5962ab9283532b
Ahmad Macbook Air <ahmaddin211056@gmail.com>
2022-03-22 10:06:44 +0700

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black':'#252526',
+        'black-2':'#2D2D30',
+        'black-3':'#3E3E42',
+        'primary':'#FF5656',
+        'success':'#1DD37B',
+        'info':'#1AA8EF'
+    },
+  },
+  plugins: [],
+}
